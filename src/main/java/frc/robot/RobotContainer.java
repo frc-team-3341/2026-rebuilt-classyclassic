9e3b04afafc0d2396e23224b4f0f18ae432106c2
TheEngineerM <isaac.schwartz.eng@gmail.com>
2026-09-12 18:06:27 -0700

    #2 Setup (Debug Upload/ is broken)
    Involves basic new outlining of the shooter subsystem class with a couple basic functions and configuration for motor control to start and some debugging to  go
    
    ISSUE: When trying to use Commands.runOnce() on a shooter subsystem function it returns and error about not being declared in the right way despite being identical to the way the template drive subsystem class does it

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -17,6 +17,7 @@ import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
 import frc.robot.commands.DriveCommands;
+import frc.robot.subsystems.shooter.Shooter;
 import frc.robot.subsystems.drive.Drive;
 import frc.robot.subsystems.drive.GyroIO;
 import frc.robot.subsystems.drive.GyroIONavX;
@@ -34,6 +35,7 @@ import org.littletonrobotics.junction.networktables.LoggedDashboardChooser;
 public class RobotContainer {
   // Subsystems
   private final Drive drive;
+  private final Shooter shooter;
 
   // Controller
   private final CommandXboxController controller = new CommandXboxController(0);
@@ -53,6 +55,7 @@ public class RobotContainer {
                 new ModuleIOSpark(1),
                 new ModuleIOSpark(2),
                 new ModuleIOSpark(3));
+        shooter = new Shooter();
         break;
 
       case SIM:
@@ -64,6 +67,7 @@ public class RobotContainer {
                 new ModuleIOSim(),
                 new ModuleIOSim(),
                 new ModuleIOSim());
+        shooter = new Shooter();
         break;
 
       default:
@@ -75,6 +79,7 @@ public class RobotContainer {
                 new ModuleIO() {},
                 new ModuleIO() {},
                 new ModuleIO() {});
+        shooter = new Shooter();
         break;
     }
 
@@ -139,6 +144,8 @@ public class RobotContainer {
                             new Pose2d(drive.getPose().getTranslation(), Rotation2d.kZero)),
                     drive)
                 .ignoringDisable(true));
+    
+    controller.a().onTrue(Commands.runOnce(() -> shooter::newPSetpoint, shooter));
   }
 
   /**
