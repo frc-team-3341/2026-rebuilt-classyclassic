5b630ee93633acdcf831adc3f9b89096cdce7023
Me <ryanh41137@gmail.com>
2026-09-19 15:51:07 -0700

    Remove unused imports in shooter subsystem. Created feeder PID constants.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -47,9 +47,16 @@ public final class Constants {
     public static final double kIflywheel = 0.0; 
     public static final double kDflywheel = 0.0; 
 
-    // Flywheel feedforward config
+    // Feeder PID configs
+    public static final double kPfeeder = 0.0001; 
+    public static final double kIfeeder = 0.0; 
+    public static final double kDfeeder = 0.0; 
+
+    // Feedforward config
     // kV: volts/rpm
     public static final double kVflywheel = 12.0/5767;
+    public static final double kVfeeder = 12.0/5767;
+
 
     public static final SparkFlexConfig FLYWHEELMOTORCONFIG = new SparkFlexConfig();
     public static final SparkFlexConfig FEEDERMOTORCONFIG = new SparkFlexConfig();
@@ -76,8 +83,21 @@ public final class Constants {
 
           FEEDERMOTORCONFIG
             .smartCurrentLimit(80)
-            .idleMode(IdleMode.kCoast);
-        
+            .idleMode(IdleMode.kBrake);
+
+          FEEDERMOTORCONFIG
+            .encoder
+            .positionConversionFactor(1)
+            .velocityConversionFactor(1);
+
+          FEEDERMOTORCONFIG.closedLoop
+            .feedbackSensor(FeedbackSensor.kPrimaryEncoder)
+            .p(kPfeeder, ClosedLoopSlot.kSlot1)
+            .i(kIfeeder, ClosedLoopSlot.kSlot1)
+            .d(kDfeeder, ClosedLoopSlot.kSlot1)
+            .outputRange(-1, 1, ClosedLoopSlot.kSlot1)
+            .feedForward
+              .kV(kVfeeder, ClosedLoopSlot.kSlot1);
       }
 
 
--- src/main/java/frc/robot/subsystems/shooter/Shooter.java
+++ src/main/java/frc/robot/subsystems/shooter/Shooter.java
@@ -8,10 +8,7 @@ import com.revrobotics.spark.FeedbackSensor;
 import com.revrobotics.spark.SparkBase.ControlType;
 import com.revrobotics.spark.SparkClosedLoopController;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
-import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkFlex;
-import com.revrobotics.spark.config.SparkMaxConfig;
-import com.revrobotics.spark.config.SparkFlexConfig;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 // Shooter Constants
